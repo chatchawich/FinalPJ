081acae3ce28ba32893c2f408e501c41c4c642db
chatchawich <44633240+chatchawich@users.noreply.github.com>
2021-01-25 17:49:16 +0700

    Update

---
+++ public/login/login.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCEPLlbn9OxnqFJrxzu6xy-f8w6td0ddjo",
+    authDomain: "sdt-psu-f1194.firebaseapp.com",
+    projectId: "sdt-psu-f1194",
+    storageBucket: "sdt-psu-f1194.appspot.com",
+    messagingSenderId: "250396603418",
+    appId: "1:250396603418:web:63c56dd1f7deb0eb560da3",
+    measurementId: "G-YN7HK3G4PB"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
