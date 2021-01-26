6e797260a5887394362e7857567cfc6c69159520
chatchawich <44633240+chatchawich@users.noreply.github.com>
2021-01-26 14:44:29 +0700

    com

--- public/addreport.js
+++ public/addreport.js
@@ -4,11 +4,14 @@ let form = document.querySelector('#addreport')
 function renderUser(doc) {
     let li = document.createElement('li')
     let report = document.createElement('span')
+    let name = document.createElement('span')
 
     li.setAttribute('data-id', doc.id);
     report.textContent = doc.data().report;
+    name.textContent = doc.data().name;
 
     li.appendChild(report);
+    li.appendChild(name);
 
     userList.appendChild(li);
 }
@@ -25,8 +28,10 @@ form.addEventListener('submit',(e) =>{
     e.preventDefault();
     db.collection('user').add({
         report: form.report.value,
+        name: form.name.value,
     })
     form.report.value ='';
+    form.name.value ='';
 });
 //real-time database
 db.collection('user').orderBy('report').onSnapshot(snapshot => {
